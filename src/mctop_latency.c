0bba927aaaae8600eaa5d6df51e39bc4afda4bc8
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2016-02-23 14:59:10 +0100

    mctop_latency bmark -- incomplete

---
+++ src/mctop_latency.c
@@ -0,0 +1,185 @@
+#include <mctop_crawler.h>
+#include <mctop.h>
+
+void ll_random_create(volatile uint64_t* mem, const size_t size);
+
+int
+main(int argc, char **argv) 
+{
+  size_t test_mem_size = 128 * 1024 * 1024LL;
+
+  struct option long_options[] = 
+    {
+      // These options don't set a flag
+      {"help",                      no_argument,       NULL, 'h'},
+      {"mem",                       required_argument, NULL, 'm'},
+      {"verbose",                   no_argument,       NULL, 'v'},
+      {NULL, 0, NULL, 0}
+    };
+
+  int i;
+  char c;
+  while(1) 
+    {
+      i = 0;
+      c = getopt_long(argc, argv, "hvn:c:r:f:s:m", long_options, &i);
+
+      if(c == -1)
+	break;
+
+      if(c == 0 && long_options[i].flag == 0)
+	c = long_options[i].val;
+
+      switch(c) 
+	{
+	case 0:
+	  /* Flag is automatically set */
+	  break;
+	case 'h':
+	  exit(0);
+	case 'm':
+	  test_mem_size = atoi(optarg);
+	  break;
+	case '?':
+	  printf("Use -h or --help for help\n");
+	  exit(0);
+	default:
+	  exit(1);
+	}
+    }
+
+
+  for (int i = 0; i < 4; i++)
+    {
+      set_cpu(i);
+      volatile uint64_t* mem = malloc(test_mem_size);
+      ll_random_create(mem, test_mem_size);
+      volatile uint64_t* l = mem;
+      const uint mem_lat_reps = 1e6;
+      volatile ticks __s = getticks();
+      for (int r = 0; r < mem_lat_reps; r++)
+	{
+	  l = (uint64_t*) *l;
+	}
+      volatile ticks __e = getticks();
+      ticks lat = (__e - __s) / mem_lat_reps;
+      printf("set_cpu@%02d : %zu\n", i, lat);
+
+      free((void*) mem);
+    }
+
+  
+  for (int i = 0; i < numa_num_task_nodes(); i++)
+    {
+      numa_run_on_node(i);
+      volatile uint64_t* mem = malloc(test_mem_size);
+      ll_random_create(mem, test_mem_size);
+      volatile uint64_t* l = mem;
+      const uint mem_lat_reps = 1e6;
+      volatile ticks __s = getticks();
+      for (int r = 0; r < mem_lat_reps; r++)
+	{
+	  l = (uint64_t*) *l;
+	}
+      volatile ticks __e = getticks();
+      ticks lat = (__e - __s) / mem_lat_reps;
+      printf("numa_run_on_node@%02d : %zu\n", i, lat);
+
+      free((void*) mem);
+    }
+
+
+  for (int x = 0; x < numa_num_task_nodes(); x++)
+    {
+      for (int i = 0; i < numa_num_task_nodes(); i++)
+	{
+	  set_cpu(x);
+	  volatile uint64_t* mem = numa_alloc_onnode(test_mem_size, i);
+	  ll_random_create(mem, test_mem_size);
+	  volatile uint64_t* l = mem;
+	  const uint mem_lat_reps = 1e6;
+	  volatile ticks __s = getticks();
+	  for (int r = 0; r < mem_lat_reps; r++)
+	    {
+	      l = (uint64_t*) *l;
+	    }
+	  volatile ticks __e = getticks();
+	  ticks lat = (__e - __s) / mem_lat_reps;
+	  printf("set_cpu(%d),allocon@%02d : %zu\n", x, i, lat);
+
+	  /* free((void*) mem); */
+	  numa_free((void*) mem, test_mem_size);
+	}
+    }
+
+
+  for (int x = 0; x < numa_num_task_nodes(); x++)
+    {
+      volatile uint64_t* mem = numa_alloc_onnode(test_mem_size, x);
+      ll_random_create(mem, test_mem_size);
+
+      for (int i = 0; i < numa_num_task_nodes(); i++)
+	{
+	  set_cpu(i);
+	  volatile uint64_t* l = mem;
+	  const uint mem_lat_reps = 1e6;
+	  volatile ticks __s = getticks();
+	  for (int r = 0; r < mem_lat_reps; r++)
+	    {
+	      l = (uint64_t*) *l;
+	    }
+	  volatile ticks __e = getticks();
+	  ticks lat = (__e - __s) / mem_lat_reps;
+	  printf("allocon(%d), set_cpu(%d) : %zu\n", x, i, lat);
+
+	  /* free((void*) mem); */
+	}
+      numa_free((void*) mem, test_mem_size);
+    }
+}
+
+
+
+
+
+/* ******************************************************************************** */
+/* help functions */
+/* ******************************************************************************** */
+
+void
+ll_random_create(volatile uint64_t* mem, const size_t size)
+{
+  const size_t size_cl = size / CACHE_LINE_SIZE;
+  const size_t per_cl = CACHE_LINE_SIZE / sizeof(uint64_t);
+  uint8_t* used = calloc_assert(size_cl, sizeof(uint8_t));
+  unsigned long* seeds = seeds_create();
+  seeds[0] = 0x0123456789ABCDLL;
+  seeds[1] = 0xA023457689BAC0LL;
+  seeds[2] = 0xB0245736F9BAC1LL;
+
+  size_t idx = 0;
+  size_t used_num = 0;
+  while (used_num < size_cl - 1)
+    {
+      used[idx] = 1;
+      used_num++;
+
+      size_t nxt;
+      do
+	{
+	  nxt = (marsaglia_rand(seeds) % size_cl);
+	}
+      while (used[nxt]);
+
+      size_t nxt_8 = (nxt * per_cl);
+      size_t idx_8 = (idx * per_cl);
+      mem[idx_8] = (uint64_t) (mem + nxt_8);
+      idx = nxt;
+    }
+
+  mem[idx * per_cl] = (uint64_t) mem;
+
+  free(seeds);
+  free(used);
+}
+
