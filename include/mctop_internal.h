66ee659f332f474421e6836dc0c4c81223c16254
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2016-04-04 16:10:06 +0200

    created mctop_internal with stuff only used in mctop, not in libmctop.a

---
+++ include/mctop_internal.h
@@ -0,0 +1,25 @@
+#ifndef __H_MCTOP_INTERNAL__
+#define __H_MCTOP_INTERNAL__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include <unistd.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  /* ******************************************************************************** */
+  /* MCTOP Cache */
+  /* ******************************************************************************** */
+
+  mctop_cache_info_t* mctop_cache_size_estimate();
+  void mctop_cache_info_free(mctop_cache_info_t* mci);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* __H_MCTOP_INTERNAL__ */
+
