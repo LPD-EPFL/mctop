7ebc0423ed20a85698b95746504f9c799213288e
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2016-04-12 14:53:58 +0200

    merge parallel std has same code as mctop_sort.c

--- tests/merge_sort/merge_sort_std_parallel.cpp
+++ tests/merge_sort/merge_sort_std_parallel.cpp
@@ -1,86 +1,208 @@
+#include <mctop_alloc.h>
 #include <pthread.h>
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <assert.h>
-#include <string.h>
-#include <sys/time.h>
-#include <malloc.h>
+#include <getopt.h>
+#include <limits.h>
+#if __x86_64__
+#  include <numaif.h>
+#endif
+
 #include <parallel/algorithm>
 #include "mctop_rand.h"
 #include "mctop_sort.h"
 
-#define min(x, y) (x<y?x:y)
-#define max(x, y) (x<y?y:x)
 
-//pthread_mutex_t global_lock;
-
-void readArray2(MCTOP_SORT_TYPE [], const long);
-void printArray2(MCTOP_SORT_TYPE [], const long, const int);
-
-MCTOP_SORT_TYPE *a __attribute__((aligned(64)));
-unsigned long *seeds;
-
-int main(int argc,char *argv[]){
-    long n, threads;
-    struct timeval start, stop;
-    unsigned long usec;
-    assert(argc >= 3);
-
-    long array_size_mb = atol(argv[1]);
-    seeds = seed_rand_fixed();
-    n = array_size_mb * 1024LL * (1024LL / sizeof(MCTOP_SORT_TYPE));
-    
-    a = (MCTOP_SORT_TYPE*) malloc(n*sizeof(MCTOP_SORT_TYPE));
-
-    threads = atoi(argv[2]);
-    assert(threads>0);
-
-    readArray2(a, n);
-
-    gettimeofday(&start, NULL);
-    //===============
-    omp_set_num_threads(threads);
-    __gnu_parallel::sort(a, a+n);
-    //==============
-    gettimeofday(&stop, NULL);
-
-    usec = (stop.tv_sec - start.tv_sec) * 1e6;
-    usec += (stop.tv_usec - start.tv_usec);
-    //printf("%d,%d,%.2f\n",n,threads,usec/(double)1e3);
-    printf("duration            : %d (ms)\n", (int)(usec/(double)1e3));
-    printArray2(a, n, 1);
-    return 0;
-}
-
-
-void readArray2(MCTOP_SORT_TYPE a[], const long limit)
+struct timespec
+timespec_diff(struct timespec start, struct timespec end)
 {
-    long i;
-    printf("Populating the array...");fflush(stdout);
-    for (i=0; i<limit; i++){
-        a[i]=i;
+  struct timespec temp;
+  if ((end.tv_nsec-start.tv_nsec) < 0)
+    {
+      temp.tv_sec = end.tv_sec-start.tv_sec-1;
+      temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
     }
-
-    // Knuth shuffle
-    for (i=limit-1; i > 0; i--){
-        long j = mctop_rand(seeds) % limit;
-        MCTOP_SORT_TYPE tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
-     }
-    printf("Done\n");fflush(stdout);
+  else
+    {
+      temp.tv_sec = end.tv_sec-start.tv_sec;
+      temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+  return temp;
 }
 
 
-void printArray2(MCTOP_SORT_TYPE *y, const long n, const int assert)
+static void
+print_error_sorted(MCTOP_SORT_TYPE* array, const size_t n_elems, const uint print_always)
 {
-    long i;
-    for(i = 0; i < n; i++){
-        if (assert)
-          assert(y[i] == (MCTOP_SORT_TYPE)i);
-        else
-          printf("x[%ld] = " MCTOP_SORT_TYPE_FORMAT " \n",i,y[i]);
+  uint sorted = 1;
+  for (size_t i = 1; i < n_elems; i++)
+    {
+      if (array[i - 1] > array[i])
+	{
+	  printf(" >>> error: array[%zu] = %u > array[%zu] = %u\n",
+		 i - 1, array[i - 1], i, array[i]);
+	  sorted = 0;
+	  break;
+	}
+    }
+  if (print_always || !sorted)
+    {
+      printf("## Array %p {size %-10zu} is sorted: %u\n", array, n_elems, sorted);
     }
 }
+
+int
+main(int argc, char **argv) 
+{
+  MCTOP_SORT_TYPE* array;
+  size_t array_len = 256 * 1024 * 1024LL;
+  /* const int array_max = 8192; */
+
+  char mct_file[100];
+  uint manual_file = 0;
+  int test_num_threads = 2;
+  int test_num_hwcs_per_socket = MCTOP_ALLOC_ALL;
+  mctop_alloc_policy test_policy = MCTOP_ALLOC_SEQUENTIAL;
+  uint test_random_type = 0;
+  uint test_verbose = 0;
+
+  struct option long_options[] = 
+    {
+      // These options don't set a flag
+      {"help",                      no_argument,             NULL, 'h'},
+      {"mct",                       required_argument,       NULL, 'm'},
+      {NULL, 0, NULL, 0}
+    };
+
+  int i;
+  char c;
+  while(1) 
+    {
+      i = 0;
+      c = getopt_long(argc, argv, "hm:n:p:c:r:s:g:i:v", long_options, &i);
+
+      if(c == -1)
+	break;
+
+      if(c == 0 && long_options[i].flag == 0)
+	c = long_options[i].val;
+
+      switch(c) 
+	{
+	case 0:
+	  /* Flag is automatically set */
+	  break;
+	case 'm':
+	  sprintf(mct_file, "%s", optarg);
+	  manual_file = 1;
+	  break;
+	case 'n':
+	  test_num_threads = atoi(optarg);
+	  break;
+	case 'c':
+	  test_num_hwcs_per_socket = atoi(optarg);
+	  break;
+	case 'p':
+	  test_policy = (mctop_alloc_policy) atoi(optarg);
+	  break;
+	case 's':
+	  {
+	    size_t array_mb = atol(optarg) * 1024 * 1024LU;
+	    array_len = array_mb / sizeof(int);
+	  }
+	  break;
+	case 'r':
+	  test_random_type = atoi(optarg);
+	  break;
+	case 'v':
+	  test_verbose = 1;
+	  break;
+	case 'h':
+	  mctop_alloc_help();
+	  exit(0);
+	case '?':
+	  printf("Use -h or --help for help\n");
+	  exit(0);
+	default:
+	  exit(1);
+	}
+    }
+
+  unsigned long* seeds = seed_rand_fixed();
+
+  const size_t array_siz = array_len * sizeof(MCTOP_SORT_TYPE);
+  array = (MCTOP_SORT_TYPE*) malloc(array_siz);
+  assert(array != NULL);
+
+  switch (test_random_type)
+    {
+    case 0:
+      printf("  // Knuth shuffle \n");
+      for (size_t i = 0; i < array_len; i++)
+	{
+	  array[i] = i;
+	}
+      for (size_t i = array_len - 1; i > 0; i--)
+	{
+	  const uint j = mctop_rand(seeds) % array_len;
+	  const MCTOP_SORT_TYPE tmp = array[i];
+	  array[i] = array[j];
+	  array[j] = tmp;
+	}
+      break;
+    case 1:
+      printf("  // Random 32bit \n");
+      for (size_t i = 0; i < array_len; i++)
+	{
+	  array[i] = mctop_rand(seeds) % (2000000000);
+	}
+      break;
+    case 2:
+      printf("  // Already sorted \n");
+      for (size_t i = 0; i < array_len; i++)
+	{
+	  array[i] = i;
+	}
+      break;
+    case 3:
+      {
+	printf("  // Already sorted - but array[0] <-> array[N-1] \n");
+	for (size_t i = 0; i < array_len; i++)
+	  {
+	    array[i] = i;
+	  }
+	MCTOP_SORT_TYPE tmp = array[0];
+	array[0] = array[array_len - 1];
+	array[array_len - 1] = tmp;
+      }
+      break;
+    case 4:
+      printf("  // Reverse sorted \n");
+      for (size_t i = 0; i < array_len; i++)
+	{
+	  array[array_len - 1 - i] = i;
+	}
+      break;
+    }
+      
+  free(seeds);
+
+  printf("# Data = %llu MB \n", array_siz / (1024 * 1024LL));
+
+  struct timespec start, stop;
+  clock_gettime(CLOCK_REALTIME, &start);
+
+  omp_set_num_threads(test_num_threads);
+  __gnu_parallel::sort(array, array + array_len);
+
+  clock_gettime(CLOCK_REALTIME, &stop);
+  struct timespec dur = timespec_diff(start, stop);
+  double dur_s = dur.tv_sec + (dur.tv_nsec / 1e9);
+  printf("%s: ## Sorted %llu MB of ints in %f seconds\n", argv[0], array_siz / (1024 * 1024LL), dur_s);
+
+  print_error_sorted(array, array_len, 0);
+
+  free((void*) array);
+
+  return 0;
+}
+
+
