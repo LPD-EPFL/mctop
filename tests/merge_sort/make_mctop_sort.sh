e13bf116a7b057d0a8f289e77f69940069f32ffe
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2016-04-13 10:05:57 +0200

    FIX: use gmake on Solaris

--- tests/merge_sort/make_mctop_sort.sh
+++ tests/merge_sort/make_mctop_sort.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
+MAKE=make;
+if [ $(uname -n) = "ol-collab1" ];
+then
+    MAKE=gmake;
+fi;
 
-make clean && SSE=0 SSE_HYPERTHREAD_RATIO=3 make -kj mctop_sort
+${MAKE} clean && SSE=0 SSE_HYPERTHREAD_RATIO=3 ${MAKE} -kj mctop_sort
 mv ./mctop_sort ./mctop_sort_no_sse
 
-make clean && SSE=1 SSE_HYPERTHREAD_RATIO=3 make -kj mctop_sort
+${MAKE} clean && SSE=1 SSE_HYPERTHREAD_RATIO=3 ${MAKE} -kj mctop_sort
 mv ./mctop_sort ./mctop_sort_sse
 
-make clean && SSE=2 SSE_HYPERTHREAD_RATIO=1 make -kj mctop_sort
+${MAKE} clean && SSE=2 SSE_HYPERTHREAD_RATIO=1 ${MAKE} -kj mctop_sort
 mv ./mctop_sort ./mctop_sort_sse_hyperthreads_1
 
-make clean && SSE=2 SSE_HYPERTHREAD_RATIO=2 make -kj mctop_sort
+${MAKE} clean && SSE=2 SSE_HYPERTHREAD_RATIO=2 ${MAKE} -kj mctop_sort
 mv ./mctop_sort ./mctop_sort_sse_hyperthreads_2
 
-make clean && SSE=2 SSE_HYPERTHREAD_RATIO=3 make -kj mctop_sort
+${MAKE} clean && SSE=2 SSE_HYPERTHREAD_RATIO=3 ${MAKE} -kj mctop_sort
 mv ./mctop_sort ./mctop_sort_sse_hyperthreads_3
 
-make clean && SSE=2 SSE_HYPERTHREAD_RATIO=4 make -kj mctop_sort
+${MAKE} clean && SSE=2 SSE_HYPERTHREAD_RATIO=4 ${MAKE} -kj mctop_sort
 mv ./mctop_sort ./mctop_sort_sse_hyperthreads_4
 
-make clean && SSE=3 SSE_HYPERTHREAD_RATIO=3 make -kj mctop_sort
+${MAKE} clean && SSE=3 SSE_HYPERTHREAD_RATIO=3 ${MAKE} -kj mctop_sort
 mv ./mctop_sort ./mctop_sort_no_sse_all_sockets
 
-make clean && make -kj merge_sort_std_parallel
+${MAKE} clean && ${MAKE} -kj merge_sort_std_parallel
 
-make clean && make -kj merge_sort_tbb_parallel
+${MAKE} clean && ${MAKE} -kj merge_sort_tbb_parallel
 
