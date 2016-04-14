c0fa1374e61a384602be677ef5cc4c46b08573fe
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2016-04-14 18:02:43 +0200

    new visual graphs of processors

--- scripts/load_and_plot.sh
+++ scripts/load_and_plot.sh
@@ -2,15 +2,15 @@
 
 print_lvl=0;
 
-un=$(uname -n);
+mct_file=desc/$(uname -n).mct
 if [ $# -gt 0 ];
 then
-    un=$1;
+    mct_file=$1;
     re='^[0-9]+$'
-    if [[ $un =~ $re ]];
+    if [[ $mct_file =~ $re ]];
     then
-	print_lvl=$un
-	un=$(uname -n);
+	print_lvl=$mct_file
+	mct_file=desc/$(uname -n).mct
     fi
     shift;
 fi;
@@ -21,6 +21,9 @@ then
     shift;
 fi;
 
+un=$(echo $mct_file | cut -d"." -f1 | sed 's/.*\///g');
+
+
 echo "#### Graphs from $un";
 
 tool=mct_load;
@@ -30,7 +33,7 @@ dot_format=ps;
 out_format=pdf
 
 make ${tool} > /dev/null;
-./${tool} -m desc/${un}.mct -l$print_lvl
+./${tool} -m $mct_file -l$print_lvl
 
 target=intra_socket
 echo "## Ploting ${target}";
--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+print_lvl=0;
+
 un=$(uname -n);
 if [ $# -gt 0 ];
 then
     un=$1;
     shift;
+fi;
+
+open=0;
+if [ $# -gt 1 ];
+then
+    open=1;
 fi;
 
 echo "#### Graphs from $un";
@@ -14,20 +22,21 @@ outf=graphs;
 dot_format=ps;
 out_format=pdf
 
-make ${tool} > /dev/null;
-./${tool} -m desc/${un}.mct -l$print_lvl
-
 target=intra_socket
 echo "## Ploting ${target}";
 dot -T${dot_format} ${inf}/dot_${target}.dot > ${outf}/${un}_${target}.${dot_format}
 epstopdf ${outf}/${un}_${target}.${dot_format}
-evince ${outf}/${un}_${target}.${out_format} &
-
+if [ $open -eq 1 ];
+then
+    evince ${outf}/${un}_${target}.${out_format} &
+fi;
 target=cross_socket
 echo "## Ploting ${target}";
 sfdp -x -T${dot_format} ${inf}/dot_${target}.dot > ${outf}/${un}_${target}.${dot_format}
 # neato -T${dot_format} ${inf}/dot_${target}.dot > ${outf}/${un}_${target}.${dot_format}
 # dot  -T${dot_format} ${inf}/dot_${target}.dot > ${outf}/${un}_${target}.${dot_format}
 epstopdf ${outf}/${un}_${target}.${dot_format}
-evince ${outf}/${un}_${target}.${out_format} &
-
+if [ $open -eq 1 ];
+then
+    evince ${outf}/${un}_${target}.${out_format} &
+fi;
